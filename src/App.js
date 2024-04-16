e0ca987d6f684537cc4c60bd4e7a673a61df9d89
Max Gordon <tonald.drump2.0@gamil.com>
2024-04-16 03:09:54 -0400

    Change logos

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ function App() {
           Hello! I'm <span className="highlight">Max Gordon</span>
         </h1>
       </div>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Faucibus purus in massa tempor nec feugiat. Blandit cursus risus at ultrices. Massa vitae tortor condimentum lacinia. Orci dapibus ultrices in iaculis nunc. Tortor condimentum lacinia quis vel. Risus commodo viverra maecenas accumsan lacus vel facilisis. Sed faucibus turpis in eu mi bibendum neque egestas congue. Pretium viverra suspendisse potenti nullam. Feugiat sed lectus vestibulum mattis ullamcorper velit. Est placerat in egestas erat imperdiet sed euismod nisi. Eget gravida cum sociis natoque. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Iaculis at erat pellentesque adipiscing commodo elit at. Molestie a iaculis at erat pellentesque adipiscing commodo. Blandit massa enim nec dui nunc mattis enim ut.
-      </p>
+      <div className="left-col">
+
+      </div>
+      <div className="right-col">
+
+      </div>
     </div>
   );
 }
