9f7361c0cd6acc7b7040a54677c31beec606ea81
Max Gordon <mgordon291us@gmail.com>
2024-11-04 17:02:55 -0500

    more debugging

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -70,32 +70,22 @@ pub fn Home() -> impl IntoView {
     let handleSubmit = move |e: SubmitEvent| {
         e.prevent_default();
 
-        debug!("1");
-        
         let potential_command = get_command(promptInput.get());
         
-        debug!("2");
-        
         if let Some(command) = potential_command {
-            debug!("3");
             writePastCmds.update(|past| {
                 past.push(view! {<p class="prompt-line">{make_prompt()}{promptInput.get()}</p>}.into_view());
                 past.push((command.function)(promptInput.get(), Box::new(move ||(writeLoadingStage.set(2)))).into_view());
             });
-            debug!("4");
         } else {
             writePastCmds.update(|past| {
                 past.push(view! {<p class="prompt-line">{make_prompt()}{promptInput.get()}</p>}.into_view());
                 past.push(view! {<CommandNotFound cmd=promptInput.get() on_finished=Box::new(move ||(writeLoadingStage.set(2))) />}.into_view());
             });
         }
-        debug!("5");
         writeLoadingStage.set(1);
-        debug!("6");
         writePromptInput.set("".to_string());
-        debug!("7");
         writeAutoComplete.set(vec![]);
-        debug!("8");
     };
 
     let s = view!{<span>"intro"</span>};
@@ -121,13 +111,13 @@ pub fn Home() -> impl IntoView {
                 <p class="prompt-line">{make_prompt()}
                 <TypeWriter html_to_type=s base_element=span() delay=200 chunk_sz=1 callback=Box::new(move ||(writeLoadingStage.set(1))) /></p>
 
-                <Show when=move || (loadingStage.get() > 0)>
+                <Show when=move || (loadingStage.get() > 0) fallback={|| view!{<p>X</p>}}>
                     <TypeWriter html_to_type=intro_text() callback=Box::new(move ||(writeLoadingStage.set(2))) />
                 </Show>
 
                 {pastCmds}
 
-                <Show when=move || (loadingStage.get() > 1)>
+                <Show when=move || (loadingStage.get() > 1) fallback={|| view!{<p>XX</p>}}>
                     <PromptInput prompt_input=promptInput on_submit=Box::new(handleSubmit) on_input=Box::new(handleInput) autocomplete=autocomplete />
                 </Show>
             </div>
